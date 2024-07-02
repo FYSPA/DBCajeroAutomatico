75e9193fb496c5c77dc0b76d0615d79cf6b15a65
Fernando <suarezfloresfy@ag.cbtis225.edu.mx>
2024-07-02 17:06:35 -0600

    Coloque un nuevo dato

--- DBCajeroAutomatico/DatosCliente.sql
+++ DBCajeroAutomatico/DatosCliente.sql
@@ -3,5 +3,6 @@
 	[Id_Cliente] INT NOT NULL PRIMARY KEY, 
     [Monto] FLOAT NULL, 
     [Saldo] FLOAT NULL, 
-    [NoCuentaPrincipal] NCHAR(10) NULL 
+    [NoCuentaPrincipal] NCHAR(10) NULL, 
+    [puntos] INT NULL 
 )
